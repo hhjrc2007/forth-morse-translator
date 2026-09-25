b9a2135efb8742d6e84eebbdf951f284ed2f5c9d
foo <hhjrc2007@proton.me>
2026-09-25 18:03:03 +0300

    add morse encoder for letters and digits

--- src/morse.fs
+++ src/morse.fs
@@ -3,8 +3,22 @@
 : letters ( -- c-addr u )
     s" .-   -... -.-. -..  .    ..-. --.  .... ..   .--- -.-  .-.. --   -.   ---  .--. --.- .-.  ...  -    ..-  ...- .--  -..- -.-- --.. " ;
 
-: morse ( n -- c-addr u ) width * letters drop + width -trailing ;
+: digits ( -- c-addr u )
+    s" -----.----..---...--....-.....-....--...---..----." ;
+
+: morse ( n -- c-addr u -- c-addr u ) drop swap width * + width -trailing ;
 
 : letter? ( c -- flag ) toupper [char] A [char] Z 1+ within ;
 : >index ( c -- n ) toupper [char] A - ;
-: .letter ( c -- ) >index morse type ;
+: .letter ( c -- ) >index letters morse type ;
+
+: numeral? ( c -- flag ) [char] 0 [char] 9 1+ within ;
+: .digit ( c -- ) [char] 0 - digits morse type ;
+
+: .char ( c -- )
+    dup bl = if drop ." / " exit then
+    dup letter? if .letter space exit then
+    dup numeral? if .digit space exit then
+    drop ;
+
+: to-morse ( "text<eol>" -- ) 0 parse bounds ?do i c@ .char loop cr ;
